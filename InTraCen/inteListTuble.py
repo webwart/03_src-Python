96bc74f792537d174d88087504dcf73ab5effbfe
Rainer Warth <rainer.warth@gmail.com>
2020-04-06 22:50:27 +0200

    main 2

--- InTraCen/inteListTuble.py
+++ InTraCen/inteListTuble.py
@@ -1,19 +1,25 @@
 import random
+import pathlib
 
+# TEST-FILE= 0001-0001_BusinessRegister-MultiEmail.csv
+
+def getLinesFromFile( lineTuple , lineList):
+    ''' lineList contains all lines from a file, lineTuple contains the start and end line of block of lines '''
+    firstLine_Block , lastLine_Block = lineTuple
+    print(str(firstLine_Block) + "--blockTEST--" + str(lastLine_Block))
+
+def readFileInList(fileName):
+    ''' return a list all lines in the input file '''
+    with open(fileName) as f:
+        return list(f)
 
 def  saveContentInNewFile(breakList):
     for  prevLastLine , b_line in breakList:
         print(str(prevLastLine).zfill(4) + "-" + str(b_line).zfill(4) + "_BusinessRegister-Email.csv")
-
-    '''
-    lastLine = b_line
-    print(str(prevLastLine) + "-"+ str(lastLine))
-    prevLastLine = b_line + 1
-    fileName  = (str(prevLastLine).zfill(4) + "-" + str(lastLine).zfill(4) + "_BusinessRegister-Email.csv")
-    file = pathlib.Path.cwd().joinpath("InTraCenTestFiles" , fileName )
-    with open(file, 'w') as nf:
-        nf.write(contentToWrite)
-    '''
+        fileName  = (str(prevLastLine).zfill(4) + "-" + str(b_line).zfill(4) + "_BusinessRegister-Email.csv")
+        file = pathlib.Path.cwd().joinpath("InTraCenTestFiles" , fileName )
+        with open(file, 'w') as nf:
+            nf.write("content in" + fileName )
 
 
 def inteListTubel( startLine , endLine , minRand , maxRand , inteList ):
@@ -38,9 +44,18 @@ def inteListTubel( startLine , endLine , minRand , maxRand , inteList ):
 
 def main():
     inteList = []
-    print(inteListTubel(599 , 5766 , 40 , 42 , inteList))
-    print("-----" * 5)
-    print(inteList)
+    print(inteListTubel(599 , 5766 , 400 , 500 , inteList))
+    print("-----saveContentInNewFile-----" * 5)
+    # print(saveContentInNewFile(inteList))
+    
+    lineList = []
+    lineListB = readFileInList("0001-0001_BusinessRegister-MultiEmail.csv")
+    for block in inteList:
+        getLinesFromFile(block, lineList )
+    
+    print("-----readFileInList-----" * 5)
+    print(lineListB)
+
 
 if __name__ == "__main__":
     main()
