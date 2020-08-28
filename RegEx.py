131dc39f7e3772caf07fbdd4551fde41903f5ec8
NapRai <rainer.warth@hotmail.ch>
2020-08-28 17:38:18 +0200

    RegEx

--- RegEx.py
+++ RegEx.py
@@ -5,12 +5,23 @@
 #   Author: Rainer Warth
 #  Version: 28-02-2020 Previous: -- First: --
 #    Goals: Learn RegEx
-#      Ref: https://docs.python.org/3.7/howto/regex.html
-#      Ref:  
+#      Ref:
+#      Ref: https://docs.python.org/3.8/howto/regex.html
+#      Ref: 
 #      Ref: 
 #    Satus: <runs> - <bug (false output , script does not run)> - <broken (link, module, file is missing)> 
 #    Satus: runs
 #       >N: --
+
+
+## Metacharacters
+# Most letters and characters will simply match themselves. There are exceptions to this rule; some characters are special "metacharacters", and don’t match themselves.
+# . ^ $ * + ? { } [ ] \ | ( )
+# If you want to match one of these characters you need to escape them with "\". In python strings "\" is however a special character. Accordingly you need add "\\" for python. 
+# Cumbersome: regEx_test = "\\\\rainer" will match "\rainer". 
+# The solution is to use Python’s raw string notation for regular expressions;
+# Better: regEx_test = r"\\rainer" 
+
 
 import re
 lines = ['555-1212', 'ILL-EGAL', '@Article{2007Azambuja,']
