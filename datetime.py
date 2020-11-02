f530396f9465763826c2b0a40366012f085a596a
SioRai <rainer.warth@gmail.com>
2020-11-02 13:58:32 +0100

    corrected date ime

--- datetime.py
+++ datetime.py
@@ -7,20 +7,21 @@
 #    Goals: learn usage of dates, time, datetime
 #      Ref: https://realpython.com/python-datetime/
 #      Ref: https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
-#    Satus: <runs> - <bug (false output , script does not run)> - <broken (link, module, file is missing)> 
-#    Satus: runs
-#       >N: --
+#    Satus: <runs> - <bug (false output , script does not run)> - <broken (link, module, file is missing)>
+#    Satus: BUG
+#   Status: def fn_timestemp( io_tag , part_1 , extension) does work. It gives me a nice file names with timestemp.
+#       >N: Why do I get: ' cannot import 'datetime' from 'datetime'. 
 #  ------------------------------------ 
  
 
 # dates are easily constructed and formatted
-from datetime import date, time, datetime
+from datetime import datetime , date , time
 today = date.today()
 print(f'With data.today I get the day: {today} !')
 
 # now.strftime("%m-%d-%y. %d %b %Y is a %A on the %d day of %B.")
 # dates support calendar arithmetic
-birthday = date(1964, 11, 27)
+birthday = datetime.date(1964, 11, 27)
 age = today - birthday
 print(age.days / 365)
 print("----- " * 3)
@@ -34,9 +35,9 @@ print(now.hour)
 print("----- " * 3)
 
 
-def fn_timestemp( part_1 , extension):
+def fn_timestemp( io_tag , part_1 , extension):
     ''' return: string , to be used as filename
-        import: from datetime
+        import: from datetime import datetime
         part_1:  <yymmdd>_<part_1>_<source>.<ext>
         extension:  extension of the filename. e.g.  .rst , .txt , .md
         The function creates a timestemp string.
@@ -45,7 +46,8 @@ def fn_timestemp( part_1 , extension):
     fn_timestemp = now.strftime("%y%m%d-%H%M")
     fn_center = part_1
     fn_ext = extension
-    return (f'{fn_timestemp}_{fn_center}.{fn_ext}')
+    fn_io = io_tag
+    return (f'{fn_io}-{fn_timestemp}_{fn_center}{fn_ext}')
 
-fn = fn_timestemp( "RecordsFromDictio" , "rst")
+fn = fn_timestemp("io" , "RecordsFromDictio" , "rst")
 print(fn)
